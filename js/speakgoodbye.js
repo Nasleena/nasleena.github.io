0fe0241d1b4cc71c07d6f4d6e0b33b8ff253d8c3
Nasleena <66716730+Nasleena@users.noreply.github.com>
2020-07-04 23:49:47 +0530

    Update speakgoodbye.js

--- js/speakgoodbye.js
+++ js/speakgoodbye.js
@@ -1,8 +1,8 @@
   
 (function (window){
   var speakgoodbye = {};
-  var greeting = "goodbye ";speakgoodbye.speak=
+  var greeting = "goodbye";speakgoodbye.speak=
     function(name){
-    console.log(greeting + name);
+    console.log(greeting + " " + name);
   }
   window.speakgoodbye = speakgoodbye;})(window);
